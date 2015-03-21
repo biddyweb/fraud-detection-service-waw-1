dfeb58d993f6af15d9b55cfc14df1e12546224b7
DanielWojda <contact@danielwojda.org>
2015-03-21 14:58:21 +0100

    Added metrics.

--- src/main/java/org/microhackaton/fraudDetection/LoanApplicationController.java
+++ src/main/java/org/microhackaton/fraudDetection/LoanApplicationController.java
@@ -1,5 +1,7 @@
 package org.microhackaton.fraudDetection;
 
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.MetricRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,16 +10,23 @@ import org.springframework.web.bind.annotation.*;
 public class LoanApplicationController {
 
     private final FraudDetectionService fraudDetectionService;
+    private final Counter goodLoanApplication;
+    private final Counter fraudLoanApplication;
+    private final Counter fishyLoanApplication;
 
     @Autowired
-    public LoanApplicationController(FraudDetectionService fraudDetectionService) {
+    public LoanApplicationController(FraudDetectionService fraudDetectionService, MetricRegistry metricRegistry) {
         this.fraudDetectionService = fraudDetectionService;
+        this.goodLoanApplication = metricRegistry.counter("frauddetection.decision.good");
+        this.fraudLoanApplication = metricRegistry.counter("frauddetection.decision.fraud");
+        this.fishyLoanApplication = metricRegistry.counter("frauddetection.decision.fishy");
     }
 
     @RequestMapping(value = "/api/loanApplication/{id}", method = RequestMethod.PUT)
         public ResponseEntity verifyLoanApplication(@PathVariable String id, @RequestBody LoanApplication loanApplication){
 
         String fraudStatus = "good";
+        goodLoanApplication.inc();
 
         FraudVerification fraudVerification = new FraudVerification(
                 loanApplication.firstName,
